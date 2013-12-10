78e03eed337312bbc27533568e1d0982c01ebc2d
rwagle <raj.wagle@ices-corporation.com>
2013-12-10 17:22:28 -0500

    Improved Hello.java on 12/10/2013

--- HelloWorld-ForGit-StartLocal/src/org/Hello.java
+++ HelloWorld-ForGit-StartLocal/src/org/Hello.java
@@ -9,6 +9,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println ("HelloWorld-ForGit-StartLocal:Hello World.");
 		System.out.println ("Ok, now with some improvements");
+		System.out.println ("More improvements 12/10/2013 @ 5:03 pm");
 
 	}
 
