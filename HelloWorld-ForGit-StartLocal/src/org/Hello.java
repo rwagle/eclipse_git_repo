e21eec4261a701e92c7f69ca820ba5ba82b7709c
rwagle <raj.wagle@ices-corporation.com>
2013-12-09 21:03:08 -0500

    Added Hello.java

---
+++ HelloWorld-ForGit-StartLocal/src/org/Hello.java
@@ -0,0 +1,14 @@
+package org;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("HelloWorld-ForGit-StartLocal:Hello World.");
+
+	}
+
+}
